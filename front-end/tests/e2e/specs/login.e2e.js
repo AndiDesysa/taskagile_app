de34510d0416e09840a5266e5f37efde4a455151
Andreas Schilling <a.schilling.1984@web.de>
2020-01-13 19:34:41 +0100

    front-end II

--- front-end/tests/e2e/specs/login.e2e.js
+++ front-end/tests/e2e/specs/login.e2e.js
@@ -5,7 +5,7 @@ module.exports = {
   'login test': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL + 'login')
-      .waitForElementVisible('#app')
+      .waitForElementVisible('#app', 5000)
       .assert.containsText('h1', 'TaskAgile')
       .end()
   }
